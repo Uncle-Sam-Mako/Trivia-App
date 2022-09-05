93f2d961af47e2373aafe81bd63049cb6c873acc
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2022-09-05 17:01:03 +0200

    update test_flaskr : Write tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -18,6 +18,8 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgresql://{}:{}@{}/{}".format("uncle-sam", "1234", "localhost:5432", self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.new_question= {"question": "Who is the black president in USA ?", "answer": "Barack Obama", "difficulty": 1, "category":4}
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -33,7 +35,56 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        """Given a web user, when he its /categories with a get request, then the response should have a status code of 200"""
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        """Given a web user when he hits /questions with a get request, then the response should have a status code of 200"""
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(len(data['total_questions']))
+        self.assertTrue(len(data['current_categorie']))
+
+    def test_get_questions_in_specific_category(self):
+        res = self.client().get('/categories/4/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['questions']), 4)
+        self.assertTrue(len(data['total_questions']))
+        self.assertTrue(len(data['current_categorie']))    
+    
+    def test_post_question(self):
+        """Given a web user, when he its /questions with a post request, then the response should have a status code of 200"""
+        res = self.client().post('/question', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_delete_question(self):
+        res = self.client().delete('/question/2')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 2).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['deleted']), 2)
+        self.assertTrue(len(data['total_questions']))
+        self.assertEqual(question, None)
+        
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
