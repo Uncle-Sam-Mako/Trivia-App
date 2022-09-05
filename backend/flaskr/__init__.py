e5172f5d0edf108f9bb68467e7b17e86f680648e
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2022-09-05 15:56:54 +0200

    Install dependencies & configure db path

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -97,6 +97,8 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
-
+    @app.route('/')
+    def index():
+        return "Hello World!! :)"
     return app
 
